6e9348d304c6b29eed1898544d4fb7afe1d905bf
CSim <100964977+C-Sim@users.noreply.github.com>
2022-07-01 21:32:51 +0100

    succesful seed

--- src/models/ProductTag.js
+++ src/models/ProductTag.js
@@ -1,6 +1,9 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+
+const Product = require("./Product");
+const Tag = require("./Tag");
 
 class ProductTag extends Model {}
 
@@ -32,7 +35,7 @@ ProductTag.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
